5e6068ddae4057e09e55d116e1519e2b7001cc07
Tomás Silva <tomas-250@outlook.com>
2022-12-07 20:08:42 +0000

    Generating board, calculating expressions

--- app/src/main/java/pt/isec/amov/tp1/Game.kt
+++ app/src/main/java/pt/isec/amov/tp1/Game.kt
@@ -1,33 +1,132 @@
 package pt.isec.amov.tp1
 
-class Game {
-    var board : ArrayList<String> = ArrayList()
+import android.util.Log
+import kotlin.random.Random
+
+class Game : java.io.Serializable{
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+
+    var board : ArrayList<ArrayList<String>> = ArrayList()
+    var expressions : HashMap<String, Int> = HashMap()
     val GAME_TIME = 60000L
     var level : Int = 1
     var points : Int = 0
+    var randomGenerator = Random(System.currentTimeMillis())
 
     constructor(){
         generateBoard()
     }
 
-    fun generateBoard() {
-        for (i in 0..4) {
-            for (j in 0..4) {
-                if (i % 2 == 0 && j % 2 == 0) {
-                    board.add((0..9).random().toString())
-                } else if (i % 2 == 1 && j % 2 == 1) {
-                    board.add(" ")
+
+    // function to generate a 5x5 board
+    // the board should look like
+    // 1 x 1 x 1
+    // x . x . x
+    // 1 x 1 x 1
+    // x . x . x
+    // 1 x 1 x 1
+    // where 1 is a random number between 1 and 9
+    // and x is a random operator between +, -, *
+    // and . is a empty space " "
+    fun generateBoard(){
+        board.clear()
+        for(i in 0..4){
+            var row : ArrayList<String> = ArrayList()
+            for(j in 0..4){
+                if(i % 2 == 0){
+                    if(j % 2 == 0){
+                        row.add(randomGenerator.nextInt(1, 10).toString())
+                    } else {
+                        row.add(getRandomOperator())
+                    }
                 } else {
-                    board.add(
-                        when ((0..3).random()) {
-                            0 -> "+"
-                            1 -> "-"
-                            2 -> "x"
-                            else -> "/"
-                        }
-                    )
+                    if(j % 2 == 0){
+                        row.add(getRandomOperator())
+                    } else {
+                        row.add(" ")
+                    }
                 }
+            }
+            board.add(row)
+        }
+        parseBoard()
+        Log.i("Asuryu", expressions.toString())
+    }
+
+    // function to get a random operator
+    fun getRandomOperator() : String{
+        var random = randomGenerator.nextInt(1, 4)
+        when(random){
+            1 -> return "+"
+            2 -> return "-"
+            3 -> return "*"
+        }
+        return "+"
+    }
+
+    fun parseBoard(){
+        expressions.clear()
+        for(i in 0..4){
+            if(i % 2 == 0){
+                var expression = ""
+                for(j in 0..4){
+                    if(j % 2 == 0){
+                        expression += board[i][j]
+                    } else {
+                        expression += board[i][j]
+                    }
+                }
+                expressions.put(expression, evaluateExpression(expression))
+            }
+        }
+
+        for(i in 0..4){
+            if(i % 2 == 0){
+                var expression = ""
+                for(j in 0..4){
+                    if(j % 2 == 0){
+                        expression += board[j][i]
+                    } else {
+                        expression += board[j][i]
+                    }
+                }
+                expressions.put(expression, evaluateExpression(expression))
             }
         }
     }
+
+    // function to evaluate an expression
+    // the expression is a string with numbers and operators
+    // example: 1+2*3
+    // the function must return the result of the expression
+    fun evaluateExpression(expression : String) : Int{
+        Log.i("Asuryu", expression)
+        var result = 0
+        var operator = '+'
+        var number = ""
+        for(i in 0 until expression.length){
+            if(expression[i] == '+' || expression[i] == '-' || expression[i] == '*'){
+                when(operator){
+                    '+' -> result += number.toInt()
+                    '-' -> result -= number.toInt()
+                    '*' -> result *= number.toInt()
+                }
+                operator = expression[i]
+                number = ""
+            } else {
+                number += expression[i]
+            }
+        }
+        when(operator){
+            '+' -> result += number.toInt()
+            '-' -> result -= number.toInt()
+            '*' -> result *= number.toInt()
+        }
+        return result
+    }
+
+
 }
--- app/src/main/java/pt/isec/amov/tp1/GameActivity.kt
+++ app/src/main/java/pt/isec/amov/tp1/GameActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.OnGestureListener
@@ -15,24 +16,32 @@ import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import pt.isec.amov.tp1.databinding.ActivityGameBinding
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(){
 
     lateinit var binding : ActivityGameBinding
+    lateinit var game : Game
+    var timeLeft : Int = 0
 
-    companion object {
-        private const val TAG = "GameActivity"
-        var game = Game()
-    }
+    var timer : CountDownTimer = object : CountDownTimer(60000, 1000) {
 
-    private val timer = object: CountDownTimer(game.GAME_TIME, 1000) {
         override fun onTick(millisUntilFinished: Long) {
             binding.timer.text = (millisUntilFinished / 1000).toString()
+            timeLeft = ((millisUntilFinished / 1000).toInt())
         }
 
         override fun onFinish() {
-            Log.i("Asuryu", "Fim do jogo") // TODO: Quando o jogo acabar, levar para uma nova atividade de final de jogo
-            showEndGameScreen();
+            binding.timer.text = "0"
+            timeLeft = 0
+            showEndGameScreen()
         }
+
+        fun stopTimer(){
+            cancel()
+        }
+    }
+
+    companion object {
+        private const val TAG = "GameActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,17 +61,59 @@ class GameActivity : AppCompatActivity() {
         } else {
             binding.playerNameIngame.text = R.string.jogador.toString()
         }
-        timer.start()
+
+
+
+        if(savedInstanceState != null){
+            game = savedInstanceState.getSerializable("game") as Game
+            timeLeft = savedInstanceState.getInt("timeLeft")
+            timer = object : CountDownTimer((timeLeft * 1000).toLong(), 1000) {
+
+                override fun onTick(millisUntilFinished: Long) {
+                    binding.timer.text = (millisUntilFinished / 1000).toString()
+                    timeLeft = ((millisUntilFinished / 1000).toInt())
+                }
+
+                override fun onFinish() {
+                    binding.timer.text = "0"
+                    timeLeft = 0
+                    showEndGameScreen()
+                }
+
+                fun stopTimer(){
+                    cancel()
+                }
+            }
+            timer.start()
+
+        } else {
+            game = Game()
+            timer.start()
+        }
+
         binding.level.text = getString(R.string.nivel_placeholder, game.level)
         binding.playerPoints.text = getString(R.string.points_placeholder, game.points)
 
-        for(i in 0..24) {
-            val id = resources.getIdentifier("piece$i", "id", packageName)
-            val button = findViewById<TextView>(id)
-            button.text = game.board[i]
+        // linearize the board
+        var board : ArrayList<String> = ArrayList()
+        for(i in 0..4){
+            for(j in 0..4){
+                board.add(game.board[i][j])
+            }
         }
 
-        binding.board.getChildAt(1)
+        // set the board
+        for(i in 0..24){
+            val id = resources.getIdentifier("piece$i", "id", packageName)
+            val piece = findViewById<TextView>(id)
+            piece.text = board[i]
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable("game", game)
+        outState.putInt("timeLeft", timeLeft)
     }
 
     override fun onBackPressed() {
@@ -71,6 +122,7 @@ class GameActivity : AppCompatActivity() {
         builder.setMessage(getString(R.string.exit_dialog_message))
         builder.setPositiveButton(getString(R.string.sim)) { _, _ ->
             super.onBackPressed()
+            timer.cancel()
         }
 
         builder.setNegativeButton(getString(R.string.nao)) { dialog, _ ->
